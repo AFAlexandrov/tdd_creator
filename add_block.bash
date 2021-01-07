6bdd73ca9751743f6ccee6db7e7cf045ef6df779
BestRamsay <a.ta.2000@mail.ru>
2021-01-07 09:20:44 +0300

    Update info about Copyright

--- add_block.bash
+++ add_block.bash
@@ -2,7 +2,7 @@
 
 
 # Copyright © 2019-2020 Feodor A. Alexandrov (feodor.alexandrov@yandex.ru)
-# Copyright © 2019-2020 Artem  I. Tarutin    ()
+# Copyright © 2019-2020 Artem  I. Tarutin    (a.ta.2000@mail.ru)
 
 # This file is subject to the terms and conditions of the GNU Lesser
 # General Public License v3. See the file LICENSE in the top level
